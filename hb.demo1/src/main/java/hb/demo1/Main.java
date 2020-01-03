18e25b99ae53400e44032b8ae408de29ac0a2ba3
imGaurav3 <37433926+imGaurav3@users.noreply.github.com>
2020-01-03 10:47:48 +0530

    CRUD using hibernate

--- hb.demo1/src/main/java/hb/demo1/Main.java
+++ hb.demo1/src/main/java/hb/demo1/Main.java
@@ -5,7 +5,14 @@ import org.hibernate.boot.Metadata;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.query.Query;
+
 import hb.demo1.model.Book;
+
+import java.util.List;
+
+import javax.persistence.TypedQuery;
+
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
@@ -15,15 +22,77 @@ public class Main {
 	private static SessionFactory sessionFactory;
 
 	public static void main(String[] args) {
-		Transaction transaction = null;
 
+		//populate();
+			
+		//delete(105);
+		
+		System.out.println("After updating..");
+		List<Book> books  =  read();
+		for (Book book : books) {
+			System.out.println(book);
+		}
+		shutdown();
+
+	}
+	
+	public static void populate() {
+		//create(101, "Halo Envoy", "Eric Nylund", 999);
+		//create(102, "Halo First", "Eric Nylund", 998);
+		//create(102, "Halo Smoke", "Kelly Gay", 997);
+		create(103, "Halo Rene", "Kelly Gay", 996);
+		create(104, "Halo Last", "Troy Dennigs", 995);
+		create(105, "Halo Silent", "Troy Dennigs", 994);
+	}
+	
+	public static void create(int id, String title, String author, int isbn) {
+		Transaction transaction = null;
 		try (Session session = getSessionFactory().openSession()) {
 			transaction = session.beginTransaction();
-			Book b = new Book(102, "Halo Envoy", "Eric Nylund", 888);
+			Book b = new Book(id, title, author, isbn);
 			session.save(b);
 			transaction.commit();
 		}
 	}
+	
+	public static List<Book> read(){
+		List<Book> books = null;
+		try (Session session = getSessionFactory().openSession()) {
+			Query<Book> q =  session.createQuery("select b from Book b", Book.class);
+			books = q.getResultList();
+		}
+		return books;
+	}
+	
+	public static void update(int id) {
+		try (Session session = getSessionFactory().openSession()) {
+			Book b = session.get(Book.class, id);
+			b.setAuthor("Troy Dennig");
+			session.getTransaction().begin();
+			session.update(b);
+			session.getTransaction().commit();
+			System.out.println(session.getTransaction().getStatus().toString());
+		}
+	}
+	
+	public static void delete(int id) {
+		try (Session session = getSessionFactory().openSession()) {
+			Transaction t  = null;
+			try {
+				Book b = session.get(Book.class, id);
+				t = session.getTransaction();
+				t.begin();
+				session.delete(b);
+				t.commit();
+			}catch(Exception ex) {
+				t.rollback();
+				ex.printStackTrace();
+			}
+			
+		}
+	}
+	
+
 
 	public static SessionFactory getSessionFactory() {
 		if (sessionFactory == null) {
@@ -33,6 +102,12 @@ public class Main {
 			sessionFactory = metadata.getSessionFactoryBuilder().build();
 		}
 		return sessionFactory;
+	}
+
+	public static void shutdown() {
+		if (registry != null) {
+			StandardServiceRegistryBuilder.destroy(registry);
+		}
 	}
 
 }
